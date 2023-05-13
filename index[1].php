f2e58a22ee7789a4e0be2fd48fd181583828b49e
AL-BASHA <95966999+ahmaddalbah@users.noreply.github.com>
2023-05-13 21:31:14 +0300

    Add files via upload

---
+++ index[1].php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__.'/pswlicence/psw.php';
+use PHPMailer\PHPMailer\PHPMailer;
+$mail = new PHPMailer;
+require __DIR__.'/app/init.php'; 
+require_once "pswofficial.php";
+
+?>
+
+
+
+
